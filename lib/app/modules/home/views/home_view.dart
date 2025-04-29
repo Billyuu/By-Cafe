18c9586604c2890da2afe52d99b626d3c608fbaf
unknown <davisabilissalimuyp@gmail.com>
2025-04-29 23:31:32 +0700

    membuat search dan container pemasaran dan menambahkan icon lainya

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
@@ -7,35 +6,209 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xff303030),
+        backgroundColor: Color(0xff303030),
         title: Text(
           'ByCafe',
           style: TextStyle(color: Colors.white),
         ),
-        // actions: [
-        //   IconButton(
-        //     onPressed: () {},
-        //     icon: Icon(Iconsax.menu_1_copy),
-        //     color: Colors.white,
-        //   )
-        // ],
-        leading: Builder(
-          builder: (context) => IconButton(
-              onPressed: () => Scaffold.of(context).openDrawer(),
-              icon: Icon(Iconsax.menu_1_copy)),
-        ),
-        
+        actions: [
+          Builder(
+            builder: (context) => IconButton(
+              onPressed: () => Scaffold.of(context).openEndDrawer(),
+              icon: Icon(Iconsax.menu_1_copy),
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
-      drawer: Drawer(),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.white,
+      endDrawer: Drawer( 
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              color: Color(0xff303030),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Iconsax.profile_tick,
+                      size: 30,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'ByCafe',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: Icon(Iconsax.home),
+                    title: Text('Home'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: Icon(Iconsax.setting_2),
+                    title: Text('Settings'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: Icon(Iconsax.logout_1),
+                    title: Text('Lougout'),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(children: [
+          TextField(
+            cursorColor: Color(0xff303030),
+            decoration: InputDecoration(
+              hintText: 'Search...',
+              hintStyle: TextStyle(color: Colors.grey),
+              prefixIcon: Icon(
+                Iconsax.search_favorite_copy,
+                color: Colors.grey,
+              ),
+              filled: true,
+              fillColor: const Color.fromARGB(255, 255, 255, 255),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(
+                    color: const Color(0xff303030)), // Saat tidak fokus
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(
+                    color: const Color(0xff303030), width: 2), // Saat fokus
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 170,
+            decoration: BoxDecoration(
+              color: Color(0xff303030),
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: ClipRRect(
+              //agar gambar mengikuti radius
+              borderRadius: BorderRadius.circular(24),
+              child: Stack(
+                //Menyusun elemen secara bebas, misalnya teks di atas gambar, ikon di sudut layar, dll.
+                children: [
+                  Opacity(
+                    //agar terlihat transparan
+                    opacity: 0.1,
+                    child: Image.network(
+                      'https://img.freepik.com/vetores-premium/padrao-sem-emenda-com-mao-desenhada-xicaras-de-cha-e-cafe-esbocadas-fundo-de-ladrilhos-da-pausa-para-o-cafe_217204-777.jpg',
+                      fit: BoxFit
+                          .cover, //mengisi gambar penuh tanpa merubah rasionya
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 15),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "We'll be back",
+                                    style: TextStyle(
+                                      fontFamily: 'pacifico',
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "we promised.",
+                                    style: TextStyle(
+                                      fontFamily: 'pacifico',
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        //gambar di kanan
+                        Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image.asset(
+                                'assets/coffe.PNG',
+                                width: 120,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            // SizedBox(
+                            //     height: 5),
+                            Text(
+                              'Cappucino Latte',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'pacifico',
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
