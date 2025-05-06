628eebe7971183fab4483e951f2ee9aeede74c29
unknown <davisabilissalimuyp@gmail.com>
2025-05-07 01:05:22 +0700

    splash sudah jadi

--- lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
+++ lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
@@ -1,60 +1,60 @@
 import 'package:get/get.dart';
-import 'package:bycafe/app/routes/app_pages.dart';
-import 'package:bycafe/app/modules/login/controllers/login_controller.dart';
+// import 'package:bycafe/app/routes/app_pages.dart';
+// import 'package:bycafe/app/modules/login/controllers/login_controller.dart';
 
 
 class SplashScreenController extends GetxController {
-final LoginController authC = Get.find<LoginController>();
+// final LoginController authC = Get.find<LoginController>();
 
-//   @override
-//   void onInit() {
-//     super.onInit();
-//     navigateToNextPage();
-//   }
+// //   @override
+// //   void onInit() {
+// //     super.onInit();
+// //     navigateToNextPage();
+// //   }
 
-//   void navigateToNextPage() async {
-//     await Future.delayed(Duration(seconds: 3)); // Tampilkan splash selama 3 detik
+// //   void navigateToNextPage() async {
+// //     await Future.delayed(Duration(seconds: 3)); // Tampilkan splash selama 3 detik
 
-//     // Cek status autentikasi pengguna
-//     User? user = authC.streamAuthStatus.value; // Ambil nilai dari stream
+// //     // Cek status autentikasi pengguna
+// //     User? user = authC.streamAuthStatus.value; // Ambil nilai dari stream
 
-//     if (user != null && user.emailVerified) {
-//       // Jika pengguna terautentikasi dan email terverifikasi
-//       Get.offAllNamed(Routes.HOME);
-//     } else {
-//       // Jika pengguna belum terautentikasi atau email belum terverifikasi
-//       Get.offAllNamed(Routes.LOGIN);
-//     }
-//   }
+// //     if (user != null && user.emailVerified) {
+// //       // Jika pengguna terautentikasi dan email terverifikasi
+// //       Get.offAllNamed(Routes.HOME);
+// //     } else {
+// //       // Jika pengguna belum terautentikasi atau email belum terverifikasi
+// //       Get.offAllNamed(Routes.LOGIN);
+// //     }
+// //   }
+// // }
+
+// // extension on Stream<User?> {
+// //   User? get value => null;
 // }
 
-// extension on Stream<User?> {
-//   User? get value => null;
+
+
+
+
+
+
+  //TODO: Implement SplashScreenController
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
 }
-
-
-
-
-
-
-
-  // //TODO: Implement SplashScreenController
-
-  // final count = 0.obs;
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  // }
-
-  // @override
-  // void onReady() {
-  //   super.onReady();
-  // }
-
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
-
-  // void increment() => count.value++;
-// }
--- lib/app/modules/splash_screen/views/splash_screen_view.dart
+++ lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -1,32 +1,89 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:bycafe/app/routes/app_pages.dart';
 import '../controllers/splash_screen_controller.dart';
 
 class SplashScreenView extends GetView<SplashScreenController> {
   const SplashScreenView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Center(
-            child: Image.asset("assets/splash.png"),
+          Image.asset(
+            'assets/splash.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.black.withOpacity(0.3),
           ),
           Positioned(
-            bottom: 30,
+            top: 120,
             left: 0,
             right: 0,
             child: Center(
               child: Text(
-                'flutter.io', // Text to display
+                'ByCafe',
                 style: TextStyle(
-                  fontSize: 20,
+                  fontFamily: 'pacifico',
+                  fontSize: 50,
                   fontWeight: FontWeight.bold,
-                  color: Colors.blue,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(3, 6),
+                      blurRadius: 6,
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ],
                 ),
               ),
+            ),
+          ),
+          Positioned(
+            bottom: 40,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                Text(
+                  'Feeling Low? Take a Sip Of Coffe',
+                  style: TextStyle(
+                    fontFamily: 'calfont',
+                    fontSize: 20,
+                    color: const Color.fromARGB(186, 255, 255, 255),
+                  ),
+                ),
+                SizedBox(height: 50),
+                SizedBox(
+                  width: 165,
+                  height: 65,
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      Get.toNamed(Routes.HOME);
+                    },
+                    label: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        fontFamily: 'calfont',
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffe57734),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
